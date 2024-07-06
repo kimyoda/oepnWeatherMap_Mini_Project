ff5956384954e2790ad04bdb3768018de46d4b77
kimyoda <rladygks1210@naver.com>
2024-07-06 20:27:51 +0900

    [feat] Api 세팅 및 연결

--- oepnweathermap_mini_project/src/App.tsx
+++ oepnweathermap_mini_project/src/App.tsx
@@ -1,7 +1,34 @@
+import { useEffect, useState } from "react";
 import "./App.css";
+import { WeatherData } from "./type/types";
+import axios from "axios";
 
-function App() {
+const api = {
+  url: process.env.REACT_APP_API_URL,
+  api_key: process.env.REACT_APP_WEATHER_API_KEY, // 여기에 오타가 있었음 (REACT_APP_WEATHER_APU_KEY -> REACT_APP_WEATHER_API_KEY)
+};
+
+const App = () => {
+  const [cityName, setCityName] = useState("seoul");
+  const [weather, setWeather] = useState<WeatherData>();
+  const [weatherName, setWeatherName] = useState("");
+
+  const getWeather = async () => {
+    const response = await axios.get(
+      `${api.url}?q=${cityName}&appid=${api.api_key}`
+    );
+
+    setWeather(response.data);
+    setWeatherName(response.data.weather[0].main);
+  };
+
+  useEffect(() => {
+    getWeather();
+  }, [cityName]);
+
+  console.log(api.url);
+  console.log(process.env);
   return <div className="App"></div>;
-}
+};
 
 export default App;
